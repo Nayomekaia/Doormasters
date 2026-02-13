fbdad55c50f7eaa2198acfa9f51e84b599b89257
Nayomekaia <nayomedoelwijt@gmail.com>
2026-02-13 14:13:43 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
 	import Topnav from '$lib/organisms/Top-nav.svelte';
+	import Bottemnav from '$lib/organisms/Bottom-nav.svelte';
+	import Footer from '$lib/organisms/Footer.svelte';
+
 
 	let { children } = $props();
 </script>
