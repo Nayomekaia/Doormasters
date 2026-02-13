0e9a42dd29e204455b2c7e23fbe83bbdf7e27062
Nayomekaia <nayomedoelwijt@gmail.com>
2026-02-13 14:00:45 +0100

    Update index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,26 @@
-// place files you want to import through the `$lib` alias in this folder.
+// Atoms
+export { default as Button } from "./atoms/Button.svelte";
+export { default as Breadcrumb } from "./atoms/Breadcrumb.svelte";
+
+// Molecules
+export { default as ErrorState } from "./molecules/ErrorState.svelte";
+export { default as SuccesState } from "./molecules/SuccesState.svelte";
+export { default as LoadingState } from "./molecules/LoadingState.svelte";
+
+
+// Organisms
+export { default as Hero } from "./organisms/Hero.svelte";
+
+
+// Templates
+
+
+
+// Images
+export { default as place } from "./assets/place.png";
+export { default as logo } from "./assets/logo.png";
+export { default as tech } from "./assets/tech.svg";
+export { default as ce } from "./assets/ce.svg";
+export { default as tuv} from "./assets/tuv-1.svg";
+export { default as tuvsud} from "./assets/tuv-2.svg";
+export { default as hygienic} from "./assets/hygienic.svg";
