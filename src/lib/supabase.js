1ce78af72bfb570796c1010fbce78d49556d6ceb
Nayomekaia <nayomedoelwijt@gmail.com>
2026-02-13 14:05:34 +0100

    data

---
+++ src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://tfszhjlbruuuzjxopsyo.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable__K8nMs0aQCzPSHcuzD3Tvw_5JIHLvNi';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
