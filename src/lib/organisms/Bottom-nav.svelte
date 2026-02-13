fa0654b1f4d7b871e1485f37f6f908b61c9f7cfb
Nayomekaia <nayomedoelwijt@gmail.com>
2026-02-13 17:43:45 +0100

    Update Bottom-nav.svelte

--- src/lib/organisms/Bottom-nav.svelte
+++ src/lib/organisms/Bottom-nav.svelte
@@ -6,22 +6,22 @@
   <section class="bottom-nav">
     <ul>
       <li>
-        <a class="menu-button {$page.url.pathname === '/' ? 'active' : ''}" href="/">HOME</a>
+        <a class="menu-button {$page.url.pathname === '/' ? 'active' : ''}" href="/404">HOME</a>
       </li>
       <li>
-        <a class="menu-button {$page.url.pathname === '/projecten' ? 'active' : ''}" href="/projecten">PROJECTEN</a>
+        <a class="menu-button {$page.url.pathname === '/projecten' ? 'active' : ''}" href="/404">PROJECTEN</a>
       </li>
       <li>
-        <a class="menu-button {$page.url.pathname === '/service' ? 'active' : ''}" href="/service">SERVICE</a>
+        <a class="menu-button {$page.url.pathname === '/service' ? 'active' : ''}" href="/404">SERVICE</a>
       </li>
       <li>
-        <a class="menu-button {$page.url.pathname === '/kennisbank' ? 'active' : ''}" href="/kennisbank">KENNISBANK</a>
+        <a class="menu-button {$page.url.pathname === '/kennisbank' ? 'active' : ''}" href="/404">KENNISBANK</a>
       </li>
       <li>
-        <a class="menu-button {$page.url.pathname === '/partners' ? 'active' : ''}" href="/partners">PARTNERS</a>
+        <a class="menu-button {$page.url.pathname === '/partners' ? 'active' : ''}" href="/404">PARTNERS</a>
       </li>
       <li>
-        <a class="menu-button {$page.url.pathname === '/over-ons' ? 'active' : ''}" href="/over-ons">OVER ONS</a>
+        <a class="menu-button {$page.url.pathname === '/over-ons' ? 'active' : ''}" href="/404">OVER ONS</a>
       </li>
       <li>
         <a class="menu-button {$page.url.pathname === '/contact' ? 'active' : ''}" href="/contact">CONTACT</a>
