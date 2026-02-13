54ef2d6664dc85f0b8855f83bff3ec03be7e3a5a
Nayomekaia <nayomedoelwijt@gmail.com>
2026-02-13 16:58:50 +0100

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,3 +4,4 @@ const SUPABASE_URL = 'https://tfszhjlbruuuzjxopsyo.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable__K8nMs0aQCzPSHcuzD3Tvw_5JIHLvNi';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
