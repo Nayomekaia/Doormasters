8fa8ad32acd07daf536172e7c1ebd7a36ef48255
Nayomekaia <nayomedoelwijt@gmail.com>
2026-02-20 17:36:25 +0100

    Update +page.server.js

--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -1,5 +1,5 @@
 import { Resend } from "resend";
-import { RESEND_API_KEY } from "$env/static/private";
+import { RESEND_API_KEY } from '$env/static/private';
 import { fail } from "@sveltejs/kit";
 
 const resend = new Resend(RESEND_API_KEY);
