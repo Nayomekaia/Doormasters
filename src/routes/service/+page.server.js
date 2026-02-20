d0680ffdbc7641691b402168fcf81951c6c8323e
Nayomekaia <nayomedoelwijt@gmail.com>
2026-02-20 13:37:32 +0100

    Update +page.server.js

--- src/routes/service/+page.server.js
+++ src/routes/service/+page.server.js
@@ -3,7 +3,8 @@ import { supabase } from '$lib/supabase.js';
 export async function load() {
   const { data, error } = await supabase
     .from('service')
-    .select('*'); 
+    .select('*')
+    .order('sort_order', { ascending: true });
 
   if (error) {
     console.error(error);
