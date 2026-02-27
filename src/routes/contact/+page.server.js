f374885a36ff2d51b992e625f115602362347524
Nayomekaia <nayomedoelwijt@gmail.com>
2026-02-27 14:03:46 +0100

    Update +page.server.js

--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -29,7 +29,7 @@ export const actions = {
     try {
       await resend.emails.send({
         from: "Doormasters <onboarding@resend.dev>",
-        to: "nayomedoelwijt@gmail.com", 
+        to: "info@door-masters.nl", 
         subject: "Nieuwe contact aanvraag",
         text: `
 Naam: ${naam} ${achternaam}
